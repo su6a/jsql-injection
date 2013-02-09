0710826d0cfb02da33dadcfe175f74c414c03075
ron190 <ron190@ymail.com>
2013-02-09 19:37:46 +0100

    Empty repository

--- src/exception/StoppableException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class StoppableException extends Exception {
-	private static final long serialVersionUID = -3573501525824167565L;
-	
-	public StoppableException(){
-		super("Execution stopped by user.");
-	}
-}
